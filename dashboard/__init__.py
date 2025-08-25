fb67412e736ac9037311e922b54cc605e3e17b38
mmadc0der <tim110702@mail.ru>
2025-08-26 00:07:40 +0300

    cleanup c:

--- dashboard/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-CRPlayer Dashboard Module
-
-Provides WebSocket server subscriber for pipeline integration.
-"""
-
-from .server import WebSocketDashboardSubscriber, create_dashboard_subscriber
-
-__all__ = ['WebSocketDashboardSubscriber', 'create_dashboard_subscriber']
