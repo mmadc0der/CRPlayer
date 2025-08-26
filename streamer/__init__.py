9f8532c6bf0c9976647b4348131669a0481f1dd4
mmadc0der <tim110702@mail.ru>
2025-08-27 01:06:31 +0300

    fix imports

--- streamer/__init__.py
+++ streamer/__init__.py
@@ -5,6 +5,6 @@ Contains GPU-accelerated Android streaming components for capturing
 and decoding game frames in real-time with hardware acceleration.
 """
 
-from .android_stream_gpu import AndroidStreamGPU
+from .android_stream_gpu import GPUAndroidStreamer
 
-__all__ = ['AndroidStreamGPU']
+__all__ = ['GPUAndroidStreamer']
